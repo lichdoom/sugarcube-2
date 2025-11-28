e3e4db7fe0143d99f27264ad262352bd7f15e7f2
lichdoom <r.mathieu1985@gmail.com>
2025-11-28 20:27:56 +0000

    return to `beforeunload`

--- src/state.js
+++ src/state.js
@@ -742,7 +742,7 @@ var State = (() => { // eslint-disable-line no-unused-vars, no-var
 	*******************************************************************************/
 
 	// Save cache to IndexedDB before the page is unloaded
-	document.addEventListener('visibilitychange', async () => {
+	document.addEventListener('beforeunload', async () => {
 		await session.backup('state', stateMarshal());
 	});
 
